386e55326bd07e96c9a13becb4199d5a05879deb
MohamedCK <abdulhakimm280@gmail.com>
2022-09-27 19:19:09 +0200

    Add `useDispatch` hook

--- src/components/Addbooks/Addbook.js
+++ src/components/Addbooks/Addbook.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import styles from './Addbook.module.css';
+import { addBook } from '../../src/redux/books/books';
 
-const AddBook = () => (
-  <div className={styles.container}>
-    <p className={styles.miniheader}>Add New Book</p>
-    <form className={styles.formContainer}>
-      <input type="text" placeholder="Enter title of the book" id="title" className={styles.input} />
-      <input type="text" placeholder="Enter author name" id="author" className={styles.input} />
-      <input type="text" placeholder="Enter category" id="category" className={styles.input} />
-      <button type="button" value="Add" label="Add" className={styles.addbutton}>Add Book</button>
-    </form>
-  </div>
-);
+// const AddBook = () => (
+
+// );
+
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const inputHandler = (e) => {
+    e.preventDefault();
+    dispatch(addBook(e.target.title.value, e.target.author.value));
+    e.target.reset();
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.miniheader}>Add New Book</p>
+      <form className={styles.formContainer} onSubmit={inputHandler}>
+        <input type="text" placeholder="Enter title of the book" id="title" className={styles.input} />
+        <input type="text" placeholder="Enter author name" id="author" className={styles.input} />
+        <button type="submit" value="Add" label="Add" className={styles.addbutton}>Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
